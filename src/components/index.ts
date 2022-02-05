6291a5356136101d61c8b94dbcd0b93896a5053d
seonglae <sungle3737@gmail.com>
2022-02-06 00:40:03 +0900

    fix: root page show 3 trees

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export * from './templates/NotionPage'
 export * from './organisms/NotionError'
 export * from './templates/ErrorPage'
-export * from './templates/ScrollPage'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { domain } from 'lib/config'
+import { resolveNotionPage } from 'lib/resolve-notion-page'
+import { NotionPage } from '@/components'
+import { PageProps } from 'lib/types'
 
-import { ScrollPage } from '@/components/templates/ScrollPage'
-import type { NextPage } from '@/types/next'
-
-const Index: NextPage = () => {
-  return <ScrollPage />
+export const getStaticProps = async () => {
+  try {
+    const props = await resolveNotionPage(domain)
+    return { props, revalidate: 10 }
+  } catch (err) {
+    console.error('page error', domain, err)
+    throw err
+  }
 }
 
-export default Index
+export default function NotionDomainPage(props: PageProps) {
+  return <NotionPage {...props} />
+}
