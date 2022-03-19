0f6fbb2dd330c4b9c494497fd3e73c37329ec1df
seonglae <sungle3737@gmail.com>
2022-03-20 01:12:20 +0900

    fix: remove default title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -36,7 +36,7 @@ function App(props: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <DefaultSeo title="Welcome!" titleTemplate={`%s · ${siteConfig.title}`} canonical={siteConfig.url + router.asPath} />
+      <DefaultSeo canonical={siteConfig.url + router.asPath} />
       <SocialProfileJsonLd
         type="Organization"
         name={siteConfig.title}
