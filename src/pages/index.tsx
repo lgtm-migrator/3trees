78b815ea0c5c9ef79212fb651c33985f362f19db
seonglae <sungle3737@gmail.com>
2022-03-20 00:10:25 +0900

    cd: 0.3.4 index page error with error page enhanced

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { domain } from 'lib/config'
+import { domain, rootNotionPageId } from 'lib/config'
 import { resolveNotionPage } from 'lib/resolve-notion-page'
 import { NotionPage } from '@/components'
 import { PageProps } from 'lib/types'
@@ -10,7 +10,7 @@ import type { GetStaticPropsResult } from 'next'
 const DAY = 3600 * 24
 
 export const getStaticProps = async () => {
-  const props = await resolveNotionPage(domain, '')
+  const props = await resolveNotionPage(domain, rootNotionPageId)
   if (!props.error) return { props, revalidate: DAY * 3 } as GetStaticPropsResult<PageProps>
   else
     return { notFound: false, revalidate: true, props: { error: props.error } } as unknown as GetStaticPropsResult<PageProps>
