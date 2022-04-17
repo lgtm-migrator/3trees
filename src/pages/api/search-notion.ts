d6a1c09382f50223004ff820a5d5c9cc8e049a7f
seonglae <sungle3737@gmail.com>
2022-04-17 18:41:23 +0900

    fix: #12 ignore template page when search

--- src/pages/api/search-notion.ts
+++ src/pages/api/search-notion.ts
@@ -10,8 +10,17 @@ const searchNotion = async (req: NextApiRequest, res: NextApiResponse) => {
   const searchParams: SearchParams = req.body
   searchParams.ancestorId = rootNotionPageId
   console.debug('lambda search-notion', searchParams)
+  if (searchParams.filters)
+    searchParams.filters = {
+      isDeletedOnly: false,
+      excludeTemplates: true,
+      isNavigableOnly: true,
+      requireEditPermissions: false,
+    }
   const results = await search(searchParams)
   results.results.map(result => (result.isNavigable = true))
+  // @ts-ignore
+  results.results = results.results.filter(result => !results.recordMap.block[result.id].is_template)
   res.setHeader('Cache-Control', 'public, s-maxage=60, max-age=60, stale-while-revalidate=60')
   res.status(200).json(results)
 }
