dd463fd4916c385b2516c2cf32d8050ec47d61ff
seonglae <sungle3737@gmail.com>
2022-04-20 22:47:44 +0900

    feat(sse): dynamic og social image added for sse optimization

--- src/components/organisms/PageHead.tsx
+++ src/components/organisms/PageHead.tsx
@@ -1,25 +1,74 @@
-import * as React from 'react'
 import Head from 'next/head'
-import { PageProps } from 'lib/types'
+import * as React from 'react'
+
+import * as types from 'lib/types'
+import * as config from 'lib/config'
 import { bgColor } from '~/site-config'
+import { getSocialImageUrl } from 'lib/get-social-image-url'
 
-// TODO: remove duplication between PageHead and NotionPage Head
+export const PageHead: React.FC<
+  types.PageProps & {
+    darkMode: boolean
+    title?: string
+    description?: string
+    image?: string
+    pageId: string
+    url?: string
+  }
+> = ({ site, title, description, pageId, image, url, darkMode }) => {
+  title = title ?? site?.name
+  description = description ?? site?.description
 
-export const PageHead: React.FC<PageProps & { darkMode: boolean }> = ({ site, darkMode }) => {
+  const socialImageUrl = getSocialImageUrl(pageId) || image
+
   return (
     <Head>
       <meta charSet="utf-8" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-      {site?.description && (
+      <meta name="theme-color" content="#EB625A" />
+      <meta property="og:type" content="website" />
+
+      {site && (
         <>
-          <meta name="description" content={site.description} />
-          <meta property="og:description" content={site.description} />
+          <meta property="og:site_name" content={site.name} />
+          <meta property="twitter:domain" content={site.domain} />
         </>
       )}
+
+      {config.twitter && <meta name="twitter:creator" content={`@${config.twitter}`} />}
+
+      {description && (
+        <>
+          <meta name="description" content={description} />
+          <meta property="og:description" content={description} />
+          <meta name="twitter:description" content={description} />
+        </>
+      )}
+
+      {socialImageUrl ? (
+        <>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content={socialImageUrl} />
+          <meta property="og:image" content={socialImageUrl} />
+        </>
+      ) : (
+        <meta name="twitter:card" content="summary" />
+      )}
+
+      {url && (
+        <>
+          <link rel="canonical" href={url} />
+          <meta property="og:url" content={url} />
+          <meta property="twitter:url" content={url} />
+        </>
+      )}
+
       <meta name="theme-color" content={darkMode ? bgColor.dark : bgColor.light} />
-      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta name="twitter:title" content={title} />
+      <title>{title}</title>
     </Head>
   )
 }
